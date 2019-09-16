d80d81dc2dc3235d4b97bbd45d59eee4a6b3895e
klok101 <blackhawks4447@gmail.com>
2019-09-15 22:12:33 -0500

    Final Pokemon Tool Box (3 Generations + 1st Stage)

--- app/src/main/java/com/example/toolbox/MainActivity.java
+++ app/src/main/java/com/example/toolbox/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.toolbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void shareInfo1(View v){
+
+        CheckBox myGen = (CheckBox) findViewById(R.id.CBgen1);
+
+        String myGenStr = myGen.getText().toString();
+
+        Intent intent = new Intent(this,secondActivity.class);
+
+        myGen.setChecked(false);
+
+        intent.putExtra(secondActivity.MY_GEN, myGenStr);
+
+        startActivity(intent);
+
+    }
+
+    public void shareInfo2(View v){
+
+        CheckBox myGen = (CheckBox) findViewById(R.id.CBgen2);
+
+        String myGenStr = myGen.getText().toString();
+
+        Intent intent = new Intent(this,secondActivity.class);
+
+        myGen.setChecked(false);
+
+        intent.putExtra(secondActivity.MY_GEN, myGenStr);
+
+        startActivity(intent);
+
+
+
+    }
+
+    public void shareInfo3(View v){
+
+        CheckBox myGen = (CheckBox) findViewById(R.id.CBgen3);
+
+        String myGenStr = myGen.getText().toString();
+
+        Intent intent = new Intent(this,secondActivity.class);
+
+        myGen.setChecked(false);
+
+        intent.putExtra(secondActivity.MY_GEN, myGenStr);
+
+        startActivity(intent);
+
     }
 }
